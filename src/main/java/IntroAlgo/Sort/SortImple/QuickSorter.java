c358024b41e6b56d5181896371661f567c8f52ef
adam <adamchen.pub@gmail.com>
2014-09-17 22:41:11 +0800

    add QuickSort

---
+++ src/main/java/IntroAlgo/Sort/SortImple/QuickSorter.java
@@ -0,0 +1,15 @@
+package IntroAlgo.Sort.SortImple;
+
+import IntroAlgo.Sort.AbsSorter.ComparableSorter;
+
+import java.util.Comparator;
+
+/**
+ * Created by adam on 14-9-17.
+ */
+public class QuickSorter extends ComparableSorter{
+    @Override
+    protected void invoke(Object[] datas, Comparator comparator) {
+
+    }
+}
